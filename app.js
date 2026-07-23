376abacb74d087886a1e5189bd3208786ba20f74
Fairouze <fairouzefaissoil25@gmail.com>
2026-07-23 10:57:24 +0000

    Application de Bonjour sur node.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+console.log("Bonjour tout le monde, bientot c'est les vacances");
+
+app.listen(3000,() =>{
+console.log("l'application demarre sur le port 3000");
+});
+
